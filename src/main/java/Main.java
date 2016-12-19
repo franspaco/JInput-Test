7ff62e8ccf3cc395c3469baa44b169a27d1f3aba
franspaco <franspaco@gmail.com>
2016-12-19 15:00:32 -0600

    First test

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+/**
+ * Created by franspaco on 19/12/16.
+ */
+
+import net.java.games.input.Controller;
+import net.java.games.input.ControllerEnvironment;
+import net.java.games.*;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("1");
+        Controller[] controllers = ControllerEnvironment.getDefaultEnvironment().getControllers();
+
+        for (Controller c : controllers) {
+            System.out.println("  " + c.getName() + " -> " + c.getType());
+        }
+    }
+}
