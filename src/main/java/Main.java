73dd378c0c23cf308463e49f4232756e6ae61a9d
franspaco <franspaco@gmail.com>
2016-12-20 01:00:59 -0600

    test in windows

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,7 @@ import net.java.games.*;
 
 public class Main {
     public static void main(String[] args){
+        System.setProperty("java.library.path");
         System.out.println("1");
         Controller[] controllers = ControllerEnvironment.getDefaultEnvironment().getControllers();
 
